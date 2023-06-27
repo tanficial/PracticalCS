3c66ad0ca852fc40b16451f0be14aa4ee068b3e7
tanficial <tanficial9574@gmail.com>
2023-06-27 23:18:34 +0900

    fix : use interface in SalesCounter class

--- SalesCalculator/SalesCounter.cs
+++ SalesCalculator/SalesCounter.cs
@@ -2,7 +2,7 @@ using System.Diagnostics.Contracts;
 
 namespace SalesCalculator;
 public class SalesCounter {
-    private List<Sale> _sales;
+    private IEnumerable<Sale> _sales;
 
     public SalesCounter(string filePath) {
         this._sales = ReadSales(filePath);
@@ -11,8 +11,8 @@ public class SalesCounter {
     // File.ReadAllLines : 작은 파일에만 쓰자. 큰 파일에는 못쓰는 함수
     // 객체 이니셜라이저 : new Class{ prop1 = 1, prop2 = 2, prop3 = 3};
     // 객체 이니셜라이저를 사용하지 않으면 라인 사이에 다른 코드가 들어갈 수 있어 가독성을 해친다.
-    private static List<Sale> ReadSales(string filePath) {
-        List<Sale> sales = new List<Sale>();
+    private static IEnumerable<Sale> ReadSales(string filePath) {
+        ICollection<Sale> sales = new List<Sale>();
         string[] lines = File.ReadAllLines(filePath);
         foreach (string line in lines) {
             string[] items = line.Split(",");
@@ -28,8 +28,8 @@ public class SalesCounter {
     }
 
     // 점포별 매출을 구한다.
-    public Dictionary<string, int> GetPerStoreSales() {
-        Dictionary<string, int> dict = new Dictionary<string, int>();
+    public IDictionary<string, int> GetPerStoreSales() {
+        IDictionary<string, int> dict = new SortedDictionary<string, int>();
 
         foreach (Sale sale in _sales) {
             if (dict.ContainsKey(sale.ShopName!)) {
