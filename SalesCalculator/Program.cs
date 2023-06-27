88cfcc68f0d1f69ca626c62226b55666871a1b12
tanficial <tanficial9574@gmail.com>
2023-06-27 23:29:43 +0900

    fix : use var keyword

--- SalesCalculator/Program.cs
+++ SalesCalculator/Program.cs
@@ -5,7 +5,15 @@ namespace SalesCalculator;
 internal class Program {
     private static void Main(string[] args)
     {
-        SalesCounter salesCounter = new SalesCounter("./sales.csv");
+        // var 사용법
+        // 우변의 변수형이 분명한 경우 사용
+        // 엄밀하게 형을 지정하지 않아도 될 경우 사용
+        // 우변의 형이 분명하지 않을 때는 X
+        // 변수 이름을 변수형에 의존하지 말 것.
+        // 변수 이름에 포함된 형이름이 맞다는 보장이 없으므로 코드 읽을 때 혼란에 빠질 수 있음.
+        // dynamic 대신 var 사용하지 말 것
+        // for, foreach문에서 루프 변수형에 사용 가능
+        var salesCounter = new SalesCounter("./sales.csv");
 
         var amountPerStore = salesCounter.GetPerStoreSales();
         foreach (KeyValuePair<string, int> obj in amountPerStore) {
--- SalesCalculator/SalesCounter.cs
+++ SalesCalculator/SalesCounter.cs
@@ -12,8 +12,8 @@ public class SalesCounter {
     // 객체 이니셜라이저 : new Class{ prop1 = 1, prop2 = 2, prop3 = 3};
     // 객체 이니셜라이저를 사용하지 않으면 라인 사이에 다른 코드가 들어갈 수 있어 가독성을 해친다.
     private static IEnumerable<Sale> ReadSales(string filePath) {
-        ICollection<Sale> sales = new List<Sale>();
-        string[] lines = File.ReadAllLines(filePath);
+        var sales = new List<Sale>();
+        var lines = File.ReadAllLines(filePath);
         foreach (string line in lines) {
             string[] items = line.Split(",");
             Sale sale = new Sale {
@@ -29,7 +29,7 @@ public class SalesCounter {
 
     // 점포별 매출을 구한다.
     public IDictionary<string, int> GetPerStoreSales() {
-        IDictionary<string, int> dict = new SortedDictionary<string, int>();
+        var dict = new SortedDictionary<string, int>();
 
         foreach (Sale sale in _sales) {
             if (dict.ContainsKey(sale.ShopName!)) {
