7d2b258f9d7d155f3a50272882e8bbda25b1ffe7
tanficial <tanficial9574@gmail.com>
2023-06-27 23:03:07 +0900

    fix : update SalesCounter Constructor

--- SalesCalculator/SalesCounter.cs
+++ SalesCalculator/SalesCounter.cs
@@ -3,15 +3,15 @@ using System.Diagnostics.Contracts;
 namespace SalesCalculator;
 public class SalesCounter {
     private List<Sale> _sales;
-    
-    public SalesCounter(List<Sale> sales) {
-        this._sales = sales;
+
+    public SalesCounter(string filePath) {
+        this._sales = ReadSales(filePath);
     }
     
     // File.ReadAllLines : 작은 파일에만 쓰자. 큰 파일에는 못쓰는 함수
     // 객체 이니셜라이저 : new Class{ prop1 = 1, prop2 = 2, prop3 = 3};
     // 객체 이니셜라이저를 사용하지 않으면 라인 사이에 다른 코드가 들어갈 수 있어 가독성을 해친다.
-    public static List<Sale> ReadSales(string filePath) {
+    private static List<Sale> ReadSales(string filePath) {
         List<Sale> sales = new List<Sale>();
         string[] lines = File.ReadAllLines(filePath);
         foreach (string line in lines) {
