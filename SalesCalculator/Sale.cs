bf37afa7c0f88653a7d8d9d9ecd8d7bf08f5fa37
tanficial <tanficial9574@gmail.com>
2023-06-27 21:59:17 +0900

    add Sale class

---
+++ SalesCalculator/Sale.cs
@@ -0,0 +1,11 @@
+namespace SalesCalculator;
+public class Sale {
+    // 점포 이름
+    public string? ShopName { get; set; }
+
+    // 상품 종류
+    public string? ProductCategory { get; set; }
+
+    // 매출액
+    public int Amount { get; set; }
+}
