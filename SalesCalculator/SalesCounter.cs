8b988efe5bf473263e884a0ba16ebd6eb736160c
tanficial <tanficial9574@gmail.com>
2023-06-27 23:00:18 +0900

    fix : move ReadFile method

--- SalesCalculator/SalesCounter.cs
+++ SalesCalculator/SalesCounter.cs
@@ -7,6 +7,25 @@ public class SalesCounter {
     public SalesCounter(List<Sale> sales) {
         this._sales = sales;
     }
+    
+    // File.ReadAllLines : 작은 파일에만 쓰자. 큰 파일에는 못쓰는 함수
+    // 객체 이니셜라이저 : new Class{ prop1 = 1, prop2 = 2, prop3 = 3};
+    // 객체 이니셜라이저를 사용하지 않으면 라인 사이에 다른 코드가 들어갈 수 있어 가독성을 해친다.
+    public static List<Sale> ReadSales(string filePath) {
+        List<Sale> sales = new List<Sale>();
+        string[] lines = File.ReadAllLines(filePath);
+        foreach (string line in lines) {
+            string[] items = line.Split(",");
+            Sale sale = new Sale {
+                ShopName = items[0],
+                ProductCategory = items[1],
+                Amount = int.Parse(items[2]),
+            };
+            sales.Add(sale);
+        }
+
+        return sales;
+    }
 
     // 점포별 매출을 구한다.
     public Dictionary<string, int> GetPerStoreSales() {
