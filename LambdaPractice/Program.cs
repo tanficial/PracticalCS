91d51dede305071da513875c8ae77b56a68623e0
tanficial <tanficial9574@gmail.com>
2023-06-28 22:49:54 +0900

    add : List & Lambda

--- LambdaPractice/Program.cs
+++ LambdaPractice/Program.cs
@@ -5,6 +5,42 @@ internal class Program {
         var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4, };
         var count = Count(numbers, n => n % 2 == 0);
         Console.WriteLine(count);
+
+        // List<T> 와 람다식
+        var list = new List<string> {
+            "Seoul", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+        };
+
+        var exists = list.Exists(s => s[0] == 'A');
+        Console.WriteLine(exists);
+
+        var name = list.Find(s => s.Length == 6);
+        Console.WriteLine(name);
+
+        var index = list.FindIndex(s => s == "Berlin");
+        Console.WriteLine(index);
+
+        var names = list.FindAll(s => s.Length <= 5);
+        foreach (var s in names) {
+            Console.WriteLine(s);
+        }
+
+        var removedCount = list.RemoveAll(s => s.Contains("on"));
+        Console.WriteLine(removedCount);
+
+        list.ForEach(s => Console.WriteLine(s));
+        // list.ForEach(Console.WriteLine);
+
+        var lowerList = list.ConvertAll(s => s.ToLower());
+        lowerList.ForEach(s => Console.WriteLine(s));
+
+        var numStrList = new List<string> {
+            "1", "2", "3",
+        };
+
+        var numList = numStrList.ConvertAll(s => int.Parse(s));
+        numList.ForEach(n => Console.WriteLine(n.GetType()));
+        numStrList.ForEach(n => Console.WriteLine(n.GetType()));
     }
 
     // Action<T1, T2>
