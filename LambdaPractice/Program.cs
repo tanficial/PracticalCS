0ad8d8adbfbb27a2a02ef27abdae1289ea0eaf79
tanficial <tanficial9574@gmail.com>
2023-06-28 19:44:17 +0900

    add : Count method

--- LambdaPractice/Program.cs
+++ LambdaPractice/Program.cs
@@ -1,9 +1,20 @@
 ﻿namespace LambdaPractice;
 
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+internal class Program {
+    private static void Main(string[] args) {
+        var count = Count(5);
+        Console.WriteLine(count);
+    }
+
+    static public int Count(int num) {
+        var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4, };
+        int count = 0;
+        foreach (var n in numbers) {
+            if (n == num) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
