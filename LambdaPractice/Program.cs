9550cb9ec377b67ed2137ea0f54899c8b43fd20b
tanficial <tanficial9574@gmail.com>
2023-06-28 22:23:03 +0900

    update : long lambda code

--- LambdaPractice/Program.cs
+++ LambdaPractice/Program.cs
@@ -3,7 +3,15 @@
 internal class Program {
     private static void Main(string[] args) {
         var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4, };
-        var count = Count(numbers, delegate(int n) { return n % 2 == 0; });
+        Predicate<int> judge = (int n) => {
+            if(n % 2 == 0) {
+                return true;
+            }
+            else{
+                return false;
+            }
+        };
+        var count = Count(numbers, judge);
         Console.WriteLine(count);
     }
 
